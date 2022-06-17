0ccb9eaf7939e1da66fb28f6240c13df89bf745d
hasunghwa <tjdghk3920@naver.com>
2022-06-17 17:47:39 +0900

    BOJ 3059

--- Python/String/3059.py
+++ Python/String/3059.py
@@ -1,1 +1,19 @@
-print('hello world')
+import sys
+input = sys.stdin.readline
+
+def sum(S):
+  ans = 0
+  char = [0] * 26
+  for i in S:
+    char[ord(i)-65] = 1
+  
+  for i in range(len(char)):
+    if char[i] == 0:
+      ans += 65 + i
+  return ans      
+    
+T = int(input())
+
+for i in range(T):
+  S = input()
+  print(sum(S[:-1]))
